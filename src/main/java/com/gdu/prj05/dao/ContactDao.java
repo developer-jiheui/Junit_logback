48a8b7116fa8cf1a8852a46ce9d9e7298c538e53
Jiheui <developer.jiheuilee@gmail.com>
2024-03-18 17:38:00 +0900

    20240318

---
+++ src/main/java/com/gdu/prj05/dao/ContactDao.java
@@ -0,0 +1,14 @@
+package com.gdu.prj05.dao;
+
+import java.util.List;
+
+import com.gdu.prj05.dto.ContactDto;
+import org.springframework.stereotype.Repository;
+
+public interface ContactDao {
+  int registerContact(ContactDto contact);
+  int modifyContact(ContactDto contact);
+  int removeContact(int contactNo);
+  List<ContactDto> getContactList();
+  ContactDto getContactByNo(int contactNo);
+}
