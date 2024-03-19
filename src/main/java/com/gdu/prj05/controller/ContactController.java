622cb5f6101ad24377d470dfecac48dfb1321e5f
Jiheui <developer.jiheuilee@gmail.com>
2024-03-19 15:45:14 +0900

    20240319

--- src/main/java/com/gdu/prj05/controller/ContactController.java
+++ src/main/java/com/gdu/prj05/controller/ContactController.java
@@ -1,13 +1,19 @@
 package com.gdu.prj05.controller;
 
+import com.gdu.prj05.dao.ContactDao;
 import com.gdu.prj05.service.ContactService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 @RequestMapping(value = "/contact")
 @RequiredArgsConstructor
 @Controller
@@ -15,17 +21,50 @@ public class ContactController {
 
     private final ContactService contactService;
 
+    //ContactController가 동작할 때 로그를 찍는 log
+    private static final Logger log = LoggerFactory.getLogger(ContactController.class);
+
     @GetMapping(value = "/list.do")
-    public String list(Model model){
+    public String list(HttpServletRequest request, Model model){
+        log.info(request.getMethod() +": "+ request.getRequestURI());
         model.addAttribute("contactList", contactService.getContactList());
         return "contact/list";
     }
 
     @GetMapping(value = "/detail.do")
-    public String detail(@RequestParam(value = "contact-no", required = false, defaultValue = "0") int contactNo,
+    public String detail(@RequestParam(value = "contact-no", required = false, defaultValue = "0")HttpServletRequest request, int contactNo,
                          Model model){
+        log.info(request.getMethod() +": "+ request.getRequestURI());
         model.addAttribute("contact", contactService.getContactByNo(contactNo));
         return "contact/detail";
     }
+
+    @GetMapping(value = "/write.do")
+    public String write(HttpServletRequest request){
+        log.info(request.getMethod() +": "+ request.getRequestURI());
+        return "contact/write";
+    }
+
+    @PostMapping(value = "/register.do")
+    public void register(HttpServletRequest request, HttpServletResponse response){
+        contactService.registerContact(request,response);
+    }
+
+    @GetMapping(value="/remove.do")
+    public void removeGet(HttpServletRequest request, HttpServletResponse response){
+        contactService.removeContact(request, response);
+    }
+    @PostMapping(value="/remove.do")
+    public void removePost(HttpServletRequest request, HttpServletResponse response){
+        contactService.removeContact(request, response);
+    }
+
+
+    @PostMapping(value="/modify.do")
+    public void modify(HttpServletRequest request, HttpServletResponse response){
+        contactService.modifyContact(request, response);
+    }
+
+
 
 }
--- src/main/test/com/gdu/prj05/ContactUnitTest.java
+++ src/main/test/com/gdu/prj05/ContactUnitTest.java
@@ -1,15 +1,20 @@
 package com.gdu.prj05;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.gdu.prj05.dao.ContactDao;
 import com.gdu.prj05.dto.ContactDto;
+
+import java.util.List;
 
 /* 1. JUnit4 를 이용한다. */
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -19,8 +24,14 @@ import com.gdu.prj05.dto.ContactDto;
  *  1) <bean>      locations="file:src/main/webapp/WEB-INF/spring/root-context.xml"
  *  2) @Bean       classes=AppConfig.class
  *  3) @Component  locations="file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml"
+ *
+ * Dao를 bean으로 만들고 root-context.xml으로 바꿔줌
  */
 @ContextConfiguration(locations={"file:src/main/webapp/WEB-INF/spring/root-context.xml"})
+
+/* 3. 메소드의 이름순으로 테스트를 수행한다.
+*/
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 
 public class ContactUnitTest {
 
@@ -32,7 +43,7 @@ public class ContactUnitTest {
   }
 
   @Test
-  public void 등록테스트() {
+  public void test01_등록테스트() {
 
     ContactDto contact = ContactDto.builder()
             .name("테스트이름")
@@ -45,4 +56,47 @@ public class ContactUnitTest {
 
   }
 
+  @Test
+  public void test02_조회테스트(){
+     int contactNO = 1;
+
+     ContactDto contact = contactDao.getContactByNo(contactNO);
+
+     assertNotNull(contact);
+
+  }
+
+  @Test
+  public void test03_수정테스트(){
+
+  ContactDto contact = ContactDto.builder()
+            .name("[수정]테스트이름")
+            .mobile("[수정]모바일")
+            .email("[수정]테스트이메일")
+            .address("[수정]테스트주소")
+            .contactNo(1)
+            .build();
+    int updateCount = contactDao.modifyContact(contact);
+    assertEquals(1, updateCount);
+
+  }
+
+  @Test
+  public void test04_목록(){
+    List<ContactDto> contactList = contactDao.getContactList();
+
+    //contactList에 들어잇는 리스트 개수가 한개이길 기대한다
+    assertEquals(1,contactList.size());
+
+  }
+
+  @Test
+  public void test05_삭제(){
+
+    int contactNo = 1;
+    int deleteCount = contactDao.removeContact(contactNo);
+
+    assertEquals(1,deleteCount);
+
+  }
 }
